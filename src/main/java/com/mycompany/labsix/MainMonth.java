3838e42c559dde13acb30ef70c448f6902e11f07
Chandana <Chandana@DESKTOP-3H97OB6>
2019-10-30 14:08:58 -0500

    lab6

---
+++ src/main/java/com/mycompany/labsix/MainMonth.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.labsix;
+
+/**
+ *
+ * @author Chandana
+ */
+public class MainMonth {
+    public static void main(String[] args) {
+        int monthNumber = 13;
+        Month objMonth = new Month(monthNumber);
+        String strMonthName = objMonth.getMonthName();
+        System.out.println("Month Name is : "+strMonthName);
+        
+    }
+}
